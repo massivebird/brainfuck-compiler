02af063df27c651fb7cdc71f89e009c1fa883ef4
massivebird <gdrakemail@gmail.com>
2024-06-02 20:49:12 -0400

    adds: dedicated header boilerplate fn

--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,31 @@ use std::io::Write;
 fn main() -> std::io::Result<()> {
     let mut buf: Vec<u8> = Vec::new();
 
-    writeln!(&mut buf, ".intel_syntax noprefix")?;
-
-    writeln!(&mut buf, ".data")?; // delimits start of program data
-    writeln!(&mut buf, ".equ SYS_EXIT, 60")?;
-    writeln!(&mut buf, ".equ EXIT_CODE_OK, 0")?;
-
-    writeln!(&mut buf, ".bss")?; // like .data, but these items are specially initialized
-    writeln!(&mut buf, ".lcomm ARRAY, 30000")?; // makes ARRAY, pointing to zero-initialized array
-                                                // of 30k bytes
-
-    writeln!(&mut buf, ".text")?; // delimits start of program instructions
-    writeln!(&mut buf, ".global _start")?; // makes _start label visible to linker
-
-    writeln!(&mut buf, "_start:")?;
-    writeln!(&mut buf, "mov rax, SYS_EXIT")?;
-    writeln!(&mut buf, "mov rdi, EXIT_CODE_OK")?;
-    writeln!(&mut buf, "syscall")?;
+    write_header_boilerplate(&mut buf).unwrap();
 
     std::fs::write("out", buf)?;
+
+    Ok(())
+}
+
+fn write_header_boilerplate(buf: &mut Vec<u8>) -> std::io::Result<()> {
+    writeln!(buf, ".intel_syntax noprefix")?;
+
+    writeln!(buf, ".data")?; // delimits start of program data
+    writeln!(buf, ".equ SYS_EXIT, 60")?;
+    writeln!(buf, ".equ EXIT_CODE_OK, 0")?;
+
+    writeln!(buf, ".bss")?; // like .data, but these items are specially initialized
+    writeln!(buf, ".lcomm ARRAY, 30000")?; // makes ARRAY, pointing to zero-initialized array
+                                           // of 30k bytes
+
+    writeln!(buf, ".text")?; // delimits start of program instructions
+    writeln!(buf, ".global _start")?; // makes _start label visible to linker
+
+    writeln!(buf, "_start:")?;
+    writeln!(buf, "mov rax, SYS_EXIT")?;
+    writeln!(buf, "mov rdi, EXIT_CODE_OK")?;
+    writeln!(buf, "syscall")?;
 
     Ok(())
 }
